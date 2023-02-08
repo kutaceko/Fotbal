854b0e4b3abb4b97c74a04ff0a494ebcb2ad53ed
kutaceko <ondrakut@seznam.cz>
2023-02-08 16:55:41 +0100

    vykreslení druhého boostu

--- Fotbal.py
+++ Fotbal.py
@@ -3,7 +3,8 @@ import random
 import sys
 roz_X = 1500
 roz_Y = 900
-boost = 0
+boost1 = 0
+boost2 = 0
 pygame.init()
 okno = pygame.display.set_mode((roz_X, roz_Y))
 hodiny = pygame.time.Clock()
@@ -21,15 +22,30 @@ while True:
             pygame.quit()
             sys.exit()
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_RSHIFT] and boost > 0.3:
-        rychlost = 2
-        boost -= 0.3
-    if keys[pygame.K_RSHIFT] == False or boost < 0.4 :
-        rychlost = 1
-        boost += 0.1
-    if boost > 136:
-        boost = 136
+    if keys[pygame.K_LSHIFT] and boost2 > 0.3:
+        rychlost2 = 2
+        boost2 -= 0.3
+    if keys[pygame.K_LSHIFT] == False or boost2 < 0.4 :
+        rychlost2 = 1
+        boost2 += 0.1
+    if boost1 > 136:
+        boost2 = 136
+
+
+
+    if keys[pygame.K_RSHIFT] and boost1 > 0.3:
+        rychlost1 = 2
+        boost1 -= 0.3
+    if keys[pygame.K_RSHIFT] == False or boost1 < 0.4 :
+        rychlost1 = 1
+        boost1 += 0.1
+    if boost1 > 136:
+        boost1 = 136
+    
     okno.fill((192,192,192))
     pygame.draw.rect(okno, (0,0,0), (roz_X - 280, roz_Y - 100, 140, 50),2)
-    gradientRect( okno, (255, 255, 0), (255, 0, 0), pygame.Rect( roz_X - 278,roz_Y - 98, boost, 46 ) )
+    gradientRect( okno, (255, 255, 0), (255, 0, 0), pygame.Rect( roz_X - 278,roz_Y - 98, boost1, 46 ) )
+
+    pygame.draw.rect(okno, (0,0,0), (140 ,roz_Y - 100, 140, 50),2)
+    gradientRect( okno, (255, 255, 0), (255, 0, 0), pygame.Rect(142,roz_Y - 98, boost2, 46 ) )
     pygame.display.update()
