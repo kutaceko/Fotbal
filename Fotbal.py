53f73fd93f627e2f838f6381aad57f32f020a572
Ondřej Kutáček <ondrakut@seznam.cz>
2023-02-15 14:35:00 +0100

    Modre ufo se hybe

--- Fotbal.py
+++ Fotbal.py
@@ -1,18 +1,22 @@
 import pygame
 import random
 import sys
-
-
-
 roz_X = 1920
 roz_Y = 1080
+velikost = 100
+poz_X = roz_X / 4
+poz_Y = roz_Y / 2 - 50
+
+poz_X2 = roz_X / 1.5
+poz_Y2 = roz_Y / 2 - 50
+
 boost1 = 0
 boost2 = 0
 
 Ufo_blue = pygame.image.load("Obrazky/UFO-blue.png")
-Ufo_blue = pygame.transform.scale(Ufo_blue, (100,100))
+Ufo_blue = pygame.transform.scale(Ufo_blue, (velikost,velikost))
 Ufo_red = pygame.image.load("Obrazky/Ufo-red.png")
-Ufo_red = pygame.transform.scale(Ufo_red, (100,100))
+Ufo_red = pygame.transform.scale(Ufo_red, (velikost,velikost))
 
 
 
@@ -33,6 +37,22 @@ while True:
             pygame.quit()
             sys.exit()
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and poz_Y > 0 :
+        poz_Y -= rychlost1
+    if keys[pygame.K_DOWN] and poz_Y < roz_Y - velikost :
+        poz_Y += rychlost1
+    if keys[pygame.K_LEFT] and poz_X > 0 :
+        poz_X -= rychlost1
+    if keys[pygame.K_RIGHT] and poz_X < roz_X - velikost :
+        poz_X += rychlost1
+    if keys[pygame.K_RSHIFT] == True :
+        rychlost1 = 2
+        rychlost1 = 2
+    if keys[pygame.K_RSHIFT] == False :
+        rychlost1 = 1
+        rychlost1 = 1
+        
+        
     if keys[pygame.K_LSHIFT] and boost2 > 0.3:
         rychlost2 = 2
         boost2 -= 0.3
@@ -60,6 +80,6 @@ while True:
     pygame.draw.rect(okno, (0,0,0), (140 ,roz_Y - 100, 140, 50),2)
     gradientRect( okno, (255, 255, 0), (255, 0, 0), pygame.Rect(142,roz_Y - 98, boost2, 46 ) )
     
-    okno.blit( Ufo_blue, (100,100))
-    okno.blit( Ufo_red, (600,100))
+    okno.blit( Ufo_blue, (poz_X,poz_Y))
+    okno.blit( Ufo_red, (poz_X2,poz_Y2))
     pygame.display.update()
